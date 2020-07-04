beb320641e5bf370036f931e03b6e101bd690baf
Yashodhan Kocharekar <yashodhan.kocharekar@securitytrails.com>
2020-07-05 00:00:21 +0530

    changed run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-docker run  -d  -p 7979:8080  -e LOAD_EX=n  -v /etc/resolv.conf:/etc/resolv.conf yashk/docker-airflow:latest
+docker run \
+-d \
+-p 7979:8080 \ 
+-e LOAD_EX=n \
+-e AWS_ACCESS_KEY_ID='placeholder' \
+-e AWS_SECRET_ACCESS_KEY='placeholder' \
+yashk/docker-airflow:latest
